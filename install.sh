3c2a553d88dd7ad9b6e71e174f0d88b04d01da61
fatih_gucuko <web.contact@shadow-imperium.com>
2021-06-29 12:45:06 +0200

    code update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "===== Installing the required linux packages ====="
-wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+# wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 apt-get install -y default-jdk default-jre openjdk-8-jdk openjdk-8-jre
 apt-get install -y adb fastboot
 # apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
